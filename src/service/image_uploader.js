92097c414b81578adc3bb32f928bf885d28074e5
KwonEna <eeena214@gmail.com>
2022-02-09 23:51:50 +0900

    Feat : 이미지 업로드

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -2,8 +2,18 @@
 // 파일을 업로드한 뒤
 // 업로드가 됐다면 서버에 있는 이미지의 url을 리턴
 class ImageUploader {
-  upload(file) {
-    return "file";
+  async upload(file) {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "dgqh0zsnf");
+    const result = await fetch(
+      "https://api.cloudinary.com/v1_1/demo/image/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+    return await result.json();
   }
 }
 
