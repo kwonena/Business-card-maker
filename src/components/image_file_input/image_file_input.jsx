c87ce6a76c81f551ac845a09eb5eface49167a05
KwonEna <eeena214@gmail.com>
2022-02-12 23:57:51 +0900

    Feat : 로딩스피너, fileName 유무에 따라 색상 변경

--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,8 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onButtonClick = (event) => {
     event.preventDefault();
@@ -10,9 +11,9 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
 
   // 함수 자체에 async를 붙여 비동기적으로 사용 가능
   const onChange = async (event) => {
-    console.log(event.target.files[0]);
+    setLoading(true);
     const uploaded = await imageUploader.upload(event.target.files[0]);
-    console.log(uploaded);
+    setLoading(false);
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url,
@@ -29,9 +30,15 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         name="file"
         onChange={onChange}
       />
-      <button className={styles.button} onClick={onButtonClick}>
-        {name || "No file"}
-      </button>
+      {!loading && (
+        <button
+          className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+          onClick={onButtonClick}
+        >
+          {name || "No file"}
+        </button>
+      )}
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
